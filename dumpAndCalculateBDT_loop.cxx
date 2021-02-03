db2876e1789ae73e2e55e364f928178a36322203
chen hui run <chenhr@lxslc714.ihep.ac.cn>
2021-02-03 22:44:02 +0800

    follow tutorial crossvalidationapp

--- dumpAndCalculateBDT_loop.cxx
+++ dumpAndCalculateBDT_loop.cxx
@@ -59,37 +59,50 @@ void dumpAndCalculateBDT_loop(){
   int n_bjet, n_tau, tau0_ntracks, tau1_ntracks, tau0_charge, tau1_charge;
   bool y1_passPID, y1_passIso, y0_passPID, y0_passIso;
 
-  TMVA::Reader *reader1 = new TMVA::Reader("!Silent");
-  TMVA::Reader *reader2 = new TMVA::Reader("!Silent");
+  TMVA::Reader *reader = new TMVA::Reader("!Silent");
+      //TMVA::Reader *reader1 = new TMVA::Reader("!Silent");
+      //TMVA::Reader *reader2 = new TMVA::Reader("!Silent");
 
   int eventNumber;
   float met_TST,sumet_TST,m_tautau,pt_yy,pt_tautau,eta_yy,dR_y_y,dR_tau_tau,tau0_pt;
   double Dmet_TST,Dsumet_TST,Dm_tautau,Dpt_yy,Dpt_tautau,Deta_yy,DdR_y_y,DdR_tau_tau,Dtau0_pt;
 
-  reader1->AddSpectator("eventNumber", &eventNumber);
-  reader1->AddVariable("met_TST", &met_TST);
-  reader1->AddVariable("sumet_TST", &sumet_TST);
-  reader1->AddVariable("m_tautau", &m_tautau);
-  reader1->AddVariable("pt_yy", &pt_yy);
-  reader1->AddVariable("pt_tautau", &pt_tautau);
-  reader1->AddVariable("eta_yy", &eta_yy);
-  reader1->AddVariable("dR_y_y", &dR_y_y);
-  reader1->AddVariable("dR_tau_tau", &dR_tau_tau);
-  reader1->AddVariable("tau0_pt", &tau0_pt);
+  reader->AddSpectator("eventNumber", &eventNumber);
+  reader->AddVariable("met_TST", &met_TST);
+  reader->AddVariable("sumet_TST", &sumet_TST);
+  reader->AddVariable("m_tautau", &m_tautau);
+  reader->AddVariable("pt_yy", &pt_yy);
+  reader->AddVariable("pt_tautau", &pt_tautau);
+  reader->AddVariable("eta_yy", &eta_yy);
+  reader->AddVariable("dR_y_y", &dR_y_y);
+  reader->AddVariable("dR_tau_tau", &dR_tau_tau);
+  reader->AddVariable("tau0_pt", &tau0_pt);
 
-  reader2->AddSpectator("eventNumber", &eventNumber);
-  reader2->AddVariable("met_TST", &met_TST);
-  reader2->AddVariable("sumet_TST", &sumet_TST);
-  reader2->AddVariable("m_tautau", &m_tautau);
-  reader2->AddVariable("pt_yy", &pt_yy);
-  reader2->AddVariable("pt_tautau", &pt_tautau);
-  reader2->AddVariable("eta_yy", &eta_yy);
-  reader2->AddVariable("dR_y_y", &dR_y_y);
-  reader2->AddVariable("dR_tau_tau", &dR_tau_tau);
-  reader2->AddVariable("tau0_pt", &tau0_pt);
+      //reader1->AddSpectator("eventNumber", &eventNumber);
+      //reader1->AddVariable("met_TST", &met_TST);
+      //reader1->AddVariable("sumet_TST", &sumet_TST);
+      //reader1->AddVariable("m_tautau", &m_tautau);
+      //reader1->AddVariable("pt_yy", &pt_yy);
+      //reader1->AddVariable("pt_tautau", &pt_tautau);
+      //reader1->AddVariable("eta_yy", &eta_yy);
+      //reader1->AddVariable("dR_y_y", &dR_y_y);
+      //reader1->AddVariable("dR_tau_tau", &dR_tau_tau);
+      //reader1->AddVariable("tau0_pt", &tau0_pt);
 
-  reader1->BookMVA("BDTG", "dataset/weights/TMVACrossValidation_BDTG_fold1.weights.xml");
-  reader2->BookMVA("BDTG", "dataset/weights/TMVACrossValidation_BDTG_fold2.weights.xml");
+      //reader2->AddSpectator("eventNumber", &eventNumber);
+      //reader2->AddVariable("met_TST", &met_TST);
+      //reader2->AddVariable("sumet_TST", &sumet_TST);
+      //reader2->AddVariable("m_tautau", &m_tautau);
+      //reader2->AddVariable("pt_yy", &pt_yy);
+      //reader2->AddVariable("pt_tautau", &pt_tautau);
+      //reader2->AddVariable("eta_yy", &eta_yy);
+      //reader2->AddVariable("dR_y_y", &dR_y_y);
+      //reader2->AddVariable("dR_tau_tau", &dR_tau_tau);
+      //reader2->AddVariable("tau0_pt", &tau0_pt);
+
+  reader->BookMVA("BDTG", "dataset/weights/TMVACrossValidation_BDTG.weights.xml");
+      //reader1->BookMVA("BDTG", "dataset/weights/TMVACrossValidation_BDTG_fold1.weights.xml");
+      //reader2->BookMVA("BDTG", "dataset/weights/TMVACrossValidation_BDTG_fold2.weights.xml");
 
 //  RReader model("dataset/weights/TMVAClassification_BDT.weights.xml");
 //  RReader model1("dataset/weights/TMVACrossValidation_BDTG_fold1.weights.xml");
@@ -183,8 +196,10 @@ void dumpAndCalculateBDT_loop(){
       dR_tau_tau = DdR_tau_tau;
       tau0_pt = Dtau0_pt; //
 
-      if((std::abs(eventNumber)/1000)%2==0) BDTG = 1000*reader1->EvaluateMVA("BDTG");
-      else if((std::abs(eventNumber)/1000)%2==1) BDTG = 1000*reader2->EvaluateMVA("BDTG");
+          //if((std::abs(eventNumber)/1000)%2==0) BDTG = 1000*reader1->EvaluateMVA("BDTG");
+          //else if((std::abs(eventNumber)/1000)%2==1) BDTG = 1000*reader2->EvaluateMVA("BDTG");
+
+      BDTG = 1000*reader->EvaluateMVA("BDTG");
 
       tOut->Fill();
     }
